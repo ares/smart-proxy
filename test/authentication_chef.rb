bf6ad9877b5438473ec4ec32d16ed551904ce2d9
Romain Vrignaud <rvrignaud@gmail.com>
2013-11-08 15:18:41 +0000

    Add test for authentication_chef

---
+++ test/authentication_chef.rb
@@ -0,0 +1,46 @@
+require 'test_helper'
+require 'webmock/test_unit'
+require 'proxy/authentication'
+require 'net/https'
+
+class AuthenticationChefTest < Test::Unit::TestCase
+
+  def setup
+    @chefauth = Proxy::Authentication::Chef.new
+    
+    #We set a few chef related settings
+    SETTINGS.stubs(:chefserver_url).returns('https://chef.example.com')
+    SETTINGS.stubs(:smartproxy_clientname).returns('testnode1')
+    SETTINGS.stubs(:smartproxy_privatekey).returns('test/fixtures/authentication/testnode1.priv')
+
+
+    testnode1_key_path = 'test/fixtures/authentication/testnode1'
+    testnode2_key_path = 'test/fixtures/authentication/testnode2'
+    testnode1_key = OpenSSL::PKey::RSA.new(File.read(testnode1_key_path+'.priv'))
+    @testnode1_pubkey = testnode1_key.public_key.to_s.gsub("\n",'\n')
+    testnode2_key = OpenSSL::PKey::RSA.new(File.read(testnode2_key_path+'.priv'))
+    @testnode2_pubkey = testnode2_key.public_key.to_s.gsub("\n",'\n')
+    
+    #we sign with the testnode1 key
+    @mybody = "ForemanRoxx"
+    hash_body = Digest::SHA256.hexdigest(@mybody)
+    @signature = Base64.encode64(testnode1_key.sign(OpenSSL::Digest::SHA256.new,hash_body)).gsub("\n",'')
+  end
+
+  def test_signing_and_checking_with_same_key_sould_work
+    # We need to mock chef-server response 
+    response = '{"public_key":"'+@testnode1_pubkey+'","name":"testnode1","admin":false,"validator":false,"json_class":"Chef::ApiClient","chef_type":"client"}'
+    stub_request(:get, 'https://chef.example.com//clients/testnode1').to_return(:body => response.to_s, :headers => {'content-type' => 'application/json'} )
+    
+    assert(@chefauth.verify_signature_request('testnode1',@signature,@mybody), "Signing and checking with same key should pass")
+  end
+
+  def test_signing_and_checking_with_2_different_keys_sould_not_work
+    # We use an other key to check signature 
+    response = '{"public_key":"'+@testnode2_pubkey+'","name":"testnode1","admin":false,"validator":false,"json_class":"Chef::ApiClient","chef_type":"client"}'
+    stub_request(:get, 'https://chef.example.com//clients/testnode1').to_return(:body => response.to_s, :headers => {'content-type' => 'application/json'} )
+    
+    assert_equal(false,@chefauth.verify_signature_request('testnode1',@signature,@mybody), "Signing and checking with different keys should not pass")
+  end
+
+end
